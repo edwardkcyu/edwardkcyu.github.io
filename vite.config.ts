17b6f06d56a65a3b3e5179032345be25eca336da
Edward Yu <edwardkcyu@gmail.com>
2025-10-14 11:30:18 +1300

    Update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/edwardkcyu.github.io/",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
